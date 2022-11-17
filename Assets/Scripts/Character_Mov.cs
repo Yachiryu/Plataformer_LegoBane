efb8c8a2f31d0766d503b788619b64378ec1fb83
unknown <Sena@MEDAPRCSGFSD939.FormacionSena.RED>
2022-11-17 16:31:37 -0500

    Lego Bane (Commit 3)
    Script inicial del salto

--- Assets/Scripts/Character_Mov.cs
+++ Assets/Scripts/Character_Mov.cs
@@ -6,10 +6,22 @@ using UnityEditor.Animations;
 public class Character_Mov : MonoBehaviour
 {
     public Animator anim;
-
     [Range(0,5)]
+   
+    Rigidbody rb;
+
+    bool jumpFloor;
+    bool jumpTrue = false;
+    public float jumpForce = 5f;
+
     public float movementSpeed;
     public float movementRotate;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     private void Update()
     {
@@ -17,5 +29,27 @@ public class Character_Mov : MonoBehaviour
         anim.SetFloat("Speed", movementDirection.z);
         transform.Translate((movementDirection * movementSpeed) * Time.deltaTime);
         transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * movementRotate, 0));
+
+        Vector3 floor = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, floor, 00.5f))
+        {
+            jumpFloor = true;
+        }
+        else
+        {
+            jumpFloor = false;
+        }
+        jumpTrue = Input.GetButtonDown("Jump");
+
+        if (jumpTrue && jumpFloor)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            anim.SetBool("Saltar", true);
+        }
+        if (!jumpTrue && jumpFloor)
+        {
+            anim.SetBool("Saltar", false);
+        }
     }
 }
