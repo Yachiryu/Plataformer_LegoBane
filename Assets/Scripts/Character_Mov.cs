19eb1f6c1477359f0d13ef6edf9edf828d9ee2f0
Yachiryu <daniel.castrilln@misena.edu.co>
2022-11-18 10:21:59 -0500

    Lego Bane (Commit 4)
    Implementacion de la mayoria de las animaciones mejoras en todo el script y el mundo

--- Assets/Scripts/Character_Mov.cs
+++ Assets/Scripts/Character_Mov.cs
@@ -6,50 +6,111 @@ using UnityEditor.Animations;
 public class Character_Mov : MonoBehaviour
 {
     public Animator anim;
+    
+    [Header("Sistema de Movimiento")]
     [Range(0,5)]
-   
-    Rigidbody rb;
-
-    bool jumpFloor;
-    bool jumpTrue = false;
-    public float jumpForce = 5f;
-
     public float movementSpeed;
     public float movementRotate;
+    public float runSpeed;
+
+    [Header("Sistema de salto")]
+    public float jumpImpulse = 5f;
+    public float raycastRange;
+    CapsuleCollider capsuleCollider;
+    Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
 
     private void Update()
     {
+        IsRunning(); // Sistema para correr
+        
+        if (IsGrounded()) // Sistema para atacar
+        {
+            IsGroundAtacking();
+        }
+        if (!IsGrounded()) 
+        {
+            IsJumpAtack();
+        }
+
+        //Debug.DrawRay(capsuleCollider.transform.position, Vector3.down, Color.red);//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        //Debug.Log(IsGrounded());// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        
+        //Movimiento y rotacion
         Vector3 movementDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
         anim.SetFloat("Speed", movementDirection.z);
         transform.Translate((movementDirection * movementSpeed) * Time.deltaTime);
         transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * movementRotate, 0));
 
-        Vector3 floor = transform.TransformDirection(Vector3.down);
-
-        if (Physics.Raycast(transform.position, floor, 00.5f))
+        // Sistema de salto
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
-            jumpFloor = true;
+            rb.velocity = Vector3.up * jumpImpulse;
+            anim.SetBool("Jump", true);
+        }
+        if (!IsGrounded()) 
+        {
+            anim.SetBool("Jump", false);
+        }
+
+    }
+
+    bool IsGrounded() 
+    {
+        return Physics.Raycast(capsuleCollider.transform.position, Vector3.down, raycastRange);
+    }
+
+    void IsRunning() 
+    {
+        Vector3 movementDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate((movementDirection * runSpeed) * Time.deltaTime);
+           // Debug.Log("IsRunning");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+            anim.SetBool("Run", true);
         }
         else
         {
-            jumpFloor = false;
-        }
-        jumpTrue = Input.GetButtonDown("Jump");
-
-        if (jumpTrue && jumpFloor)
-        {
-            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-            anim.SetBool("Saltar", true);
-        }
-        if (!jumpTrue && jumpFloor)
-        {
-            anim.SetBool("Saltar", false);
+            anim.SetBool("Run", false);
+            //Debug.Log("IsNotRunning");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
         }
     }
+
+    void IsGroundAtacking()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("GroundAtk", true);
+            //Debug.Log("IsAtacking");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        }
+        if (!Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("GroundAtk", false);
+            //Debug.Log("IsNotAtacking");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        }
+
+
+    }
+    void IsJumpAtack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("JumpAtack", true);
+            Debug.Log("IsJumpAtacking");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        }
+        if (!Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("JumpAtack", false);
+            Debug.Log("IsNotJumpAtacking");//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        }
+    }
+
+
+
 }
