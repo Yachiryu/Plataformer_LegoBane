0dfbdb71e01512fd785cf33a91efd48ae69fb336
Yachiryu <daniel.castrilln@misena.edu.co>
2022-11-21 10:49:09 -0500

    Lego Bane (Commit 7)
    Implementacion de las condiciones de victoria, inicio de la creacion del segundo nivel

--- Assets/Scripts/ScorePoints.cs
+++ Assets/Scripts/ScorePoints.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScorePoints : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class ScorePoints : MonoBehaviour
     void Start()
     {
         puntaje = 0;
+    }
+    private void Update()
+    {
+        if (puntaje >= 3)
+        {
+            SceneManager.LoadScene("Level2");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
